e8ff05e85a342f32f15af60c608083279709e061
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2026-02-11 23:48:54 +0100

    Remove the `basePath` configuration from `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/logistics',
   // If your repository name is not 'logistics', update the basePath to your repo name
   // basePath: '/logistics', 
 };
